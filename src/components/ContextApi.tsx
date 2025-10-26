255b79c954c6d23d23415adffee3da93441ecbca
javierhrzgt <eljavierhrz@gmail.com>
2025-10-26 08:21:18 -0600

    Tipado de useReducer y Context API en TypeScript

---
+++ src/components/ContextApi.tsx
@@ -0,0 +1,37 @@
+import { useContext, createContext, useState } from 'react'
+
+type ThemeContextType = {
+    theme: string;
+    toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType>();
+
+function ThemeProvider({ children }:{children: React.ReactNode}):React.ReactNode {
+    const [theme, setTheme] = useState('light')
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+    }
+    
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+function ThemeButton() {
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    return (
+        <button
+            onClick={toggleTheme}
+            style={{
+                backgroundColor: theme === 'light' ? '#fff' : '#333',
+                color: theme === 'light' ? '#000' : '#fff',
+            }}>
+        Cambiar tema
+        </button>
+    )
+}
+
+export { ThemeProvider, ThemeButton }
